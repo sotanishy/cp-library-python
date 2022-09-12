3e4efc15e6f7a80cb60a67d2657bda3e555df560
Sota Nishiyama <sotanishy@AdansonsMBPSota.lan>
2022-09-12 23:12:20 +0900

    update weighted uf

--- weighted_union_find.py
+++ weighted_union_find.py
@@ -1,15 +1,14 @@
-class UnionFind:
+class WeightedUnionFind:
     def __init__(self, N):
-        self.par = list(range(N))
-        self.size = [1] * N
+        self.par = [-1] * N
         self.weight = [0] * N
 
     def find(self, x):
         stack = [x]
-        while self.par[x] != x:
+        while self.par[x] >= 0:
             x = self.par[x]
             stack.append(x)
-        r = stack[-1]
+        r = stack.pop()
         while stack:
             x = stack.pop()
             self.weight[x] += self.weight[self.par[x]]
@@ -20,21 +19,20 @@ class UnionFind:
         w += self.weight[x] - self.weight[y]
         x = self.find(x)
         y = self.find(y)
-        if x == y:
-            return False
-        if self.size[x] < self.size[y]:
+        if self.par[x] > self.par[y]:
             x, y = y, x
             w = -w
+        self.par[x] += self.par[y]
         self.par[y] = x
-        self.size[x] += self.size[y]
         self.weight[y] = w
-        return True
 
     def same(self, x, y):
         return self.find(x) == self.find(y)
 
     def diff(self, x, y):
-        return self.weight[self.find(y)] - self.weight[self.find(x)]
+        self.find(x)
+        self.find(y)
+        return self.weight[y] - self.weight[x]
 
-    def get_size(self, x):
-        return self.size[self.find(x)]
+    def size(self, x):
+        return -self.par[self.find(x)]
